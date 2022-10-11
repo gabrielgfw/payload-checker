9ac41ffe77c7b949448b830d1a85326a96b4fd12
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2022-10-11 00:18:09 -0300

    criando página para mocks e função de mapeamento de campos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { Layout, Input, Menu, Button, Form } from 'antd';
+
+const { Header, Footer, Content } = Layout;
+const { TextArea } = Input;
+
+const headerItems = [{
+  key: '1',
+  label: 'Principal'
+}];
 
 function App() {
+  const [payloadsExemplo, setPayloadsExemplo] = useState([]);
+   
+  function handlerAddPayloadExemplo(payload) {
+    setPayloadsExemplo([ ...payloadsExemplo, payload ]);
+  }
+
+  function onFinish(values) {
+    console.log(`# Values:`);
+    const { payloadInput } = values;
+    const payloadParsed = JSON.parse(payloadInput);
+    examineObject(payloadParsed);
+    console.log(JSON.parse(payloadInput));
+
+  }
+
+  function examineObject(payloadParsed) {
+    for(let prop in payloadParsed) {
+      const propType = returnPropertyType(payloadParsed[prop]);
+      if(propType == 'object' || propType == 'array') {
+        console.log(`# INICIO: ${prop} (${propType})`);
+        examineObject(payloadParsed[prop]);
+        console.log(`# FIM: ${prop} (${propType})`);
+      } else {
+        console.log(`# CAMPO: ${prop} (${propType})`);
+      }
+    }
+  }
+
+  function returnPropertyType(prop) {
+    if(Array.isArray(prop)) return 'array';
+    return typeof(prop);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Layout style={{ height: '100vh'}}>
+        <Header>
+          <div className="logo"/>
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            items={ headerItems }
+          />
+        </Header>
+        <Content style={{ padding: '50px', height: '100%' }}>
+          <div className="site-layout-content">
+            <Form
+              name="payloadTeste"
+              onFinish={onFinish}
+            >
+              <Form.Item name="payloadInput">
+                <TextArea rows={4} placeholder="Cole um payload de exemplo"/>
+              </Form.Item>
+              
+              <Form.Item>
+                <Button 
+                  style={{ marginTop: '10px'}} 
+                  type="primary"
+                  htmlType="submit"
+                >
+                  Adicionar
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Gabriel Felipe Werner - 2022</Footer>
+      </Layout>
+    </>
   );
 }
 

1f3020ad436f7f2330ba506e04551148b3e023a4
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2022-10-17 23:33:46 -0300

    simplificando arrays e adicionando table para mostras os campos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css';
-import { Layout, Input, Menu, Button, Form } from 'antd';
+import { Layout, Input, Menu, Button, Form, Table } from 'antd';
+import reportWebVitals from './reportWebVitals';
 
 const { Header, Footer, Content } = Layout;
 const { TextArea } = Input;
@@ -10,33 +11,97 @@ const headerItems = [{
   label: 'Principal'
 }];
 
+
+
+
 function App() {
-  const [payloadsExemplo, setPayloadsExemplo] = useState([]);
-   
-  function handlerAddPayloadExemplo(payload) {
-    setPayloadsExemplo([ ...payloadsExemplo, payload ]);
-  }
+  const [propList, setPropList] = useState([{
+    propertyName: 'rateios',
+    type: 'array',
+    description: '',
+    required: false
+  }]);
+
+  const columns = [
+    {
+      title: 'Campo',
+      dataIndex: 'propertyName',
+      key: 'propertyName',
+    },
+    {
+      title: 'Tipo',
+      dataIndex: 'type',
+      key: 'type',
+    },
+    {
+      title: 'Descrição',
+      dataIndex: 'description',
+      key: 'description',
+    },
+    {
+      title: 'Obrigatório',
+      dataIndex: 'required',
+      key: 'required',
+    }
+  ];
 
   function onFinish(values) {
-    console.log(`# Values:`);
     const { payloadInput } = values;
     const payloadParsed = JSON.parse(payloadInput);
-    examineObject(payloadParsed);
-    console.log(JSON.parse(payloadInput));
-
+    reduceArraysForSimplicity(payloadParsed);
+    const examined = examinePayload(payloadParsed, []);
+    setPropList([ ...examined ]);
   }
 
-  function examineObject(payloadParsed) {
+
+  function reduceArraysForSimplicity(payloadParsed) {
     for(let prop in payloadParsed) {
       const propType = returnPropertyType(payloadParsed[prop]);
-      if(propType == 'object' || propType == 'array') {
-        console.log(`# INICIO: ${prop} (${propType})`);
-        examineObject(payloadParsed[prop]);
-        console.log(`# FIM: ${prop} (${propType})`);
-      } else {
-        console.log(`# CAMPO: ${prop} (${propType})`);
+      if(propType === 'array') {
+        if(payloadParsed[prop].length > 0) {
+          payloadParsed[prop] = payloadParsed[prop].slice(0, 1);
+        }
       }
     }
+  }
+
+  function examinePayload(payloadParsed, propertiesStack) {
+    let properties = propertiesStack;
+
+    for(let prop in payloadParsed) {
+      const propType = returnPropertyType(payloadParsed[prop]);
+      const propZerada = prop == 0;
+
+      if(propType === 'object' || propType === 'array') {
+        properties.push({
+          propertyName: propZerada ? '-- início --' : `${prop} (INÍCIO)`,
+          type: propType,
+          description: ''
+        });
+        examinePayload(payloadParsed[prop], properties);
+
+        properties.push({
+          propertyName: propZerada ? '-- fim --' : `${prop} (FIM)`,
+          type: propType,
+          description: ''
+        });
+
+      } else {
+        properties.push({
+          propertyName: prop,
+          type: propType,
+          description: ''
+        });
+      }
+    }
+
+    return applyIndexValue(properties);
+  }
+
+  function applyIndexValue(propertiesList) {
+    return propertiesList.map((prop, index) => {
+      return { key: index+1, ...prop };
+    });
   }
 
   function returnPropertyType(prop) {
@@ -71,10 +136,19 @@ function App() {
                   type="primary"
                   htmlType="submit"
                 >
-                  Adicionar
+                  Examinar Payload
                 </Button>
               </Form.Item>
             </Form>
+            <Table 
+              dataSource={propList} 
+              columns={columns} 
+              sticky={true} 
+              pagination={false} 
+              scroll={{ y: '50vh'}} 
+              bordered={true}
+              size={'small'}
+            />
           </div>
         </Content>
         <Footer style={{ textAlign: 'center' }}>Gabriel Felipe Werner - 2022</Footer>
